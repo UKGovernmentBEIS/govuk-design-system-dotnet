d0b9fc25c06d814c76d8d3776f8b5aba408be7bd
Dan Corder <dan.corder@softwire.com>
2023-07-21 15:38:12 +0100

    feat: Update HTML to match GOV.UK Fontend v4.2.0

--- GovUkDesignSystem/GovUkDesignSystemComponents/DateInputItemViewModel.cs
+++ GovUkDesignSystem/GovUkDesignSystemComponents/DateInputItemViewModel.cs
@@ -30,11 +30,6 @@ namespace GovUkDesignSystem.GovUkDesignSystemComponents
         public string Autocomplete { get; set; }
 
         /// <summary>
-        /// 	Attribute to provide a regular expression pattern, used to match allowed character combinations for the input value.
-        /// </summary>
-        public string Pattern { get; set; } = "[0-9]*";
-
-        /// <summary>
         ///     Classes to add to date input item.
         /// </summary>
         public string Classes { get; set; }

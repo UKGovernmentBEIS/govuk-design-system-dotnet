30bc6eb5332889057b1bc9b9db06c0041574ce5a
Dan Corder <dan.corder@softwire.com>
2023-07-24 13:35:03 +0100

    refactor: removed unused using

--- GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/PaginationTests.cs
+++ GovUkDesignSystem.SnapshotTests/GovUkDesignSystemComponents/PaginationTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using GovUkDesignSystem.GovUkDesignSystemComponents;
 using GovUkDesignSystem.SnapshotTests.Helpers;

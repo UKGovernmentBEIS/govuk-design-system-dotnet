3aab4bfe389c38a9547e3fdb7bc6f1b3c14d04bf
Glenn Clarke <glenn.clarke@softwire.com>
2025-01-06 10:16:41 +0000

    Add a custom validation attribute

---
+++ GovUkDesignSystem/Attributes/ValidationAttributes/GovUkValidateCustom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GovUkDesignSystem.Attributes.ValidationAttributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class GovUkValidateCustom: ValidationAttribute
+{
+    public string CustomValidationPropertyName;
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var isRequiredPropertyInfo = validationContext.ObjectInstance.GetType().GetProperty(CustomValidationPropertyName);
+            
+        if (isRequiredPropertyInfo is null)
+        {
+            throw new ArgumentException(
+                $"'{CustomValidationPropertyName}' must be a boolean property in the model the attribute is included in");
+        }
+            
+        var isValid = (bool)isRequiredPropertyInfo.GetValue(validationContext.ObjectInstance, null)!;
+            
+        return isValid ? new ValidationResult(ErrorMessage) : ValidationResult.Success;
+    }
+}
